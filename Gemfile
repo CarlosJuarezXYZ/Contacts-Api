f192d4e86630dbdb706c845b5f271a9209eaa9de
unknown <carlostuna015@gmail.com>
2021-06-09 18:41:42 -0500

    gem pg to production

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'rack-cors'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -49,5 +51,8 @@ group :test do
   gem 'webdrivers'
 end
 
+group :production do
+  gem 'pg'
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@ test_user = User.create(email: "test@mail.com", password: "123456")
 
 contacts = [
           { name: 'Andrew Clark', email: 'andrew@react.com', number: '975318642',
-            relation: 'Work', birthday: '1995-05-01', urlImage: 'dfgdfgdfgdgf', gender: 'Masculine' },
+            relation: 'Work', birthday: '1995-05-01', urlImage: 'https://www.quever.news/u/fotografias/m/2020/9/12/f850x638-1140_78629_4825.jpg', gender: 'Masculine' },
           { name: 'Brian Vaughn', email: 'brian@react.com', number: '987654321',
-            relation: 'Friends',  birthday: '1995-05-01', urlImage: 'dfgdfgdfgdgf', gender: 'Masculine' },
+            relation: 'Friends',  birthday: '1995-05-01', urlImage: 'https://img.europapress.es/fotoweb/fotonoticia_20200404154752_1200.jpg', gender: 'Masculine' },
           { name: 'Dan Abramov', email: 'dan@react.com', number: '123456789', relation: 'Friends',
-          birthday: '1995-05-01', urlImage: 'dfgdfgdfgdgf', gender: 'Masculine' },
+          birthday: '1995-05-01', urlImage: 'https://i.pinimg.com/originals/d9/d0/39/d9d03922a0509ed034eb91f91cde856d.jpg', gender: 'Masculine' },
           { name: 'Dominic Gannaway', email: 'dominic@react.com', number: '135792468',
-            relation: 'Acquaintance',  birthday: '1995-05-01', urlImage: 'dfgdfgdfgdgf', gender: 'Masculine' },
+            relation: 'Acquaintance',  birthday: '1995-05-01', urlImage: 'https://www.tonica.la/__export/1606087293449/sites/debate/img/2020/11/22/batman-ranking-actores.jpg_423682103.jpg', gender: 'Masculine' },
           { name: 'Luna Ruan', email: 'luna@react.com', number: '222333444', relation: 'Work',
-          birthday: '1995-05-01', urlImage: 'dfgdfgdfgdgf', gender: 'Female' }
+          birthday: '1995-05-01', urlImage: 'https://static.wikia.nocookie.net/marvelpeliculas/images/d/d3/280px-Natasha_Romanoff_Avengers.jpg/revision/latest/scale-to-width-down/280?cb=20121203210332&path-prefix=es', gender: 'Female' }
         ]
 
 contacts.each { |contact| test_user.contacts.create(contact) }
